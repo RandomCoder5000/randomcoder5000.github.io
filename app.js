e45c5654747b9cd2f23984886052ffbc5b33bacd
RandomCoder5000 <33358103+RandomCoder5000@users.noreply.github.com>
2017-11-30 16:08:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+var today = 'date';
 
-document.getElementById('heading').innerHTML = localStorage['title'] || 'Just Write';
+document.getElementById('heading').innerHTML = localStorage['title'] || 'Homework for Today - '+ today;
 document.getElementById('content').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) ';
 
  setInterval(function() {
