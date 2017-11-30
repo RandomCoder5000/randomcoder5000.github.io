a76db2337c1e75411ec5abcc3ab9dbecabdf1a31
RandomCoder5000 <33358103+RandomCoder5000@users.noreply.github.com>
2017-11-30 16:24:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
 
+if(dd<10) {
+    dd = '0'+dd
+}
 
-var today = 'date';
+if(mm<10) {
+    mm = '0'+mm
+}
+
+today = mm + '/' + dd + '/' + yyyy
+document.write(today);
 
 document.getElementById('heading').innerHTML = localStorage['title'] || 'Homework for Today - '+ today;
-document.getElementById('content').innerHTML = localStorage['text'] || 'English - <br> Math - <br>';
+document.getElementById('content').innerHTML = localStorage['text'] || 'English - <br> Math - <br> Physics - <br> Latin - <br> Religion - <br> World Civ - <br> Music - <br> Health - <br> Guidance - <br> Homeroom - ';
 
  setInterval(function() {
       localStorage['title'] = document.getElementById('heading').innerHTML;
