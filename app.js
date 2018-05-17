8fdcb0422094998fb48b8648462ec89ad91a2aed
RandomCoder5000 <33358103+RandomCoder5000@users.noreply.github.com>
2018-05-17 17:00:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-
-if(dd<10) {
-    dd = '0'+dd
-}
-
-if(mm<10) {
-    mm = '0'+mm
-}
-
-today = mm + '/' + dd + '/' + yyyy
-document.write(today);
-
 document.getElementById('heading').innerHTML = localStorage['title'] || 'Homework';
-document.getElementById('content').innerHTML = localStorage['text'] || 'Class 1 - <br> Class 2 - <br> Class 3 - <br> Class 4 - <br> Class 5 - <br> Class 6 - <br> Class  7 - <br> Class 8 - <br> Class 9 - <br> Class 10 - <br> Class 11 <br> Class 12 <br> Homeroom - ';
+document.getElementById('content').innerHTML = localStorage['text'] || 'IMPORTANT: <br> Class 1 - <br> Class 2 - <br> Class 3 - <br> Class 4 - <br> Class 5 - <br> Class 6 - <br> Class  7 - <br> Class 8 - <br> Class 9 - <br> Class 10 - <br> Class 11 <br> Class 12 <br> Homeroom - ';
 
+document.getElementById('heading').style.fontweight = "700";
 
 setInterval(function() { 
       localStorage['title'] = document.getElementById('heading').innerHTML;
