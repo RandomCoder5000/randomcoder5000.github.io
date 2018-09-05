56857081456585d1080419da52d8bdcedc612aa0
RandomCoder5000 <33358103+RandomCoder5000@users.noreply.github.com>
2018-09-05 17:59:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('heading').innerHTML = localStorage['title'] || 'Homework';
-document.getElementById('content').innerHTML = localStorage['text'] || 'IMPORTANT: <br> Class 1 - <br> Class 2 - <br> Class 3 - <br> Class 4 - <br> Class 5 - <br> Class 6 - <br> Class  7 - <br> Homeroom - ';
+document.getElementById('content').innerHTML = localStorage['text'] || 'IMPORTANT: <br> Class 1 - <br> Class 2 - <br> Class 3 - <br> Class 4 - <br> Class 5 - <br> Class 6 - <br> Class  7 - <br> Class 8 - <br> Homeroom - ';
 
 document.getElementById('heading').style.fontweight = "700";
 
